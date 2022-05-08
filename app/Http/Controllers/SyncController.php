7e73776a8ef32d9507a2763355abc3fe6dde78b5
Caen De Silva <caen@desilva.se>
2022-05-08 20:36:48 +0200

    Create SyncController.php

---
+++ app/Http/Controllers/SyncController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SyncController extends Controller
+{
+    /**
+     * Download and sync the records.
+     * Can be called from Artisan or a job.
+     */
+    public function __invoke()
+    {
+        //
+    }
+}
