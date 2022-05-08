8bee37ea52477bf97c45a10cf1ce75f1b3c983d1
Caen De Silva <caen@desilva.se>
2022-05-08 22:32:39 +0200

    Add unique lines to chart

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,35 +43,34 @@
         <canvas id="myChart" width="400" height="400" style="max-width: 800px; max-height: 400px;"></canvas>
        
         <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
-
+     
 <script>
 const ctx = document.getElementById('myChart');
+
+var total = {
+    label: "Total",
+    data: {!! json_encode($clonesHyde->pluck('total')) !!},
+    lineTension: 0,
+    fill: false,
+    borderColor: 'green'
+  };
+
+var unique = {
+    label: "Unique",
+    data: {!! json_encode($clonesHyde->pluck('unique')) !!},
+    lineTension: 0,
+    fill: false,
+  borderColor: 'blue'
+  };
+
+var data = {
+  labels: {!! json_encode($clonesHyde->pluck('date')) !!},
+  datasets: [total, unique]
+};
+
 const myChart = new Chart(ctx, {
     type: 'line',
-    data: {
-        labels: {!! json_encode($clonesHyde->pluck('date')) !!},
-        datasets: [{
-            label: '# of Votes',
-            data: {!! json_encode($clonesHyde->pluck('total')) !!},
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data: data,
     options: {
         scales: {
             y: {
