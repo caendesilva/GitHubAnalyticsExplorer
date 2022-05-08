780b23268199614c62c91285ef5f102d1328b1fa
Caen De Silva <caen@desilva.se>
2022-05-08 21:54:35 +0200

    Add basic clones table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -15,7 +16,7 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         return view('welcome', [
-            //
+            'events' => Event::all(),
         ]);
     }
 }
