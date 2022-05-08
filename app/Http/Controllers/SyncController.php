fac01d79dc8ce7269dd16547d5b2845c60441aa8
Caen De Silva <caen@desilva.se>
2022-05-08 21:11:28 +0200

    Create controller to sync records with the API

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Event;
+use Illuminate\Support\Facades\Http;
 
 class SyncController extends Controller
 {
@@ -12,6 +13,44 @@ class SyncController extends Controller
      */
     public function __invoke()
     {
-        //
+        // Sync clone traffic for hydephp/framework
+        echo '<pre>';
+    
+        echo "Syncing clone traffic for hydephp/framework\n";    
+        $this->syncClones('hydephp/framework', 'traffic/clones');
+    }
+
+
+    protected function syncClones($repository, $type)
+    {
+        echo "Requesting response from GitHub\n";
+        $response = Http::withToken(config('services.github.token'))->withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'User-Agent' => 'github.com/caendesilva',
+        ])->get('https://api.github.com/repos/' . $repository . '/' . $type);
+
+        echo "Got response: ".$response->status() ." \n";
+
+        foreach($response->object()->clones as $clone) {
+            $this->syncEvent($repository, $type, $clone);
+        }
+    }
+  
+    protected function syncEvent($repository, $type, $event)
+    {
+        echo " > Syncing event: ".$event->timestamp."\n";
+        $event = Event::where([
+            'repository' => $repository,
+            'type' => $type,
+            'bucket' => $event->timestamp,
+        ])->firstOrCreate([
+            'repository' => $repository,
+            'type' => $type,
+            'bucket' => $event->timestamp,
+            'total' => $event->count,
+            'unique' => $event->uniques,
+        ]);
+
+        $event->save();
     }
 }
