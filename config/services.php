229f4c140eabd5ab29b70820dfa832c036d76a2f
Caen De Silva <caen@desilva.se>
2022-05-08 21:11:06 +0200

    Add GitHub token to services

--- config/services.php
+++ config/services.php
@@ -1,3 +1,4 @@
+
 <?php
 
 return [
@@ -29,6 +30,10 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'github' => [
+        'token' => env('GITHUB_TOKEN'),
     ],
 
 ];
