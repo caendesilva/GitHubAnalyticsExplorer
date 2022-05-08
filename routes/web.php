771545d1d8e7daba1ace9b55a35ef111993b8648
Caen De Silva <caen@desilva.se>
2022-05-08 21:20:45 +0200

    Add HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SyncController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', HomeController::class);
 Route::get('/sync', SyncController::class);
