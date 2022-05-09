dcb9ed6fe45ddc546a9925d5f8586f82009bfe5e
Caen De Silva <caen@desilva.se>
2022-05-09 12:21:34 +0200

    Add JSON links + download

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -13,25 +13,38 @@
             padding: 4px 8px;
             font-family: monospace;
         }
-
 		th {
             text-transform: uppercase;
 		}
-
         table {
             border-collapse: collapse;
-        }
-
-        form {
-            margin: 0px;
         }
         h1 > small {
             font-size: 16px;
         }
+        menu {
+            display: flex;
+            list-style: none;
+            padding: 0;
+            font-size: 16px;
+        }
+        menu li {
+            margin-right: 12px;
+        }
     </style>
 </head>
 <body>
-    <h1>HydePHP Raw Data</h1>
+    <h1>
+        HydePHP Raw Data
+    </h1>
+    <menu role="toolbar">
+        <li>
+            <a href="/table.json">View JSON</a>
+        </li>
+        <li>
+            <a href="/table.json" download>Download JSON</a>
+        </li>
+    </menu>
 	<table>
         <thead>
             <tr>
