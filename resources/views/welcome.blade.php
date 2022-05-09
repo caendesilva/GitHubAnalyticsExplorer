ad7de405b2d3051119d039493625812656aa9703
Caen De Silva <caen@desilva.se>
2022-05-09 13:04:46 +0200

    Add header information and layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,23 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 </head>
 <body>
-    <h1>HydePHP Statistics</h1>
+    <header style="text-align: center; margin: 60px 40px;">
+        <h1>HydePHP Statistics</h1>
+        <h2>
+            HydePHP is open source, its data should be too.
+        </h2>
+        <p>
+            HydePHP is a Laravel-powered Static App Generator that
+            allows you to create blogs, documentation sites, and more.
+        </p>
+        <p>
+            Learn more about Hyde, and try it out for yourself, at
+            <a href="https://hydephp.github.io/">https://hydephp.github.io/</a>! 
+        </p>
+    </header>
 
-    <main style="display: flex;">
-        <section style="min-width: 800px;">
+    <main style="display: flex; justify-content: center; overflow-x: auto;">
+        <section style="min-width: 800px; border: 1px solid lightgray;">
             <h2 style="text-align: center;">Daily GitHub clone activity per repository</h2>
             <div style="padding: 10px 20px;">
                 <x-clones-chart :clones="$clonesHyde" />
@@ -22,7 +35,7 @@
             </div>
         </section>
     
-        <section style="min-width: 800px;">
+        <section style="min-width: 800px; border: 1px solid lightgray;">
             <h2 style="text-align: center;">Daily GitHub view activity per repository</h2>
             <div style="padding: 10px 20px;">
                 <x-views-chart :views="$viewsHyde" />
