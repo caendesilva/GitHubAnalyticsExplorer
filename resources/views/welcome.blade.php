5c971c3977e455762897cc9ad871975c0299329c
Caen De Silva <caen@desilva.se>
2022-05-09 13:28:16 +0200

    Add navigation menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HydePHP Statistics</title>
+    <style>
+        nav {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        nav ul li {
+            float: left;
+        }
+        nav ul li a {
+            display: block;
+            text-align: center;
+            padding: 8px 12px;
+            text-decoration: none;
+            background: gainsboro;
+            border-right: 1px solid gray;
+            overflow: visible;
+        }
+        nav ul li a:hover, nav ul li a:focus, nav ul li a.active {
+            background: lightgray;
+        }
+        nav ul li:last-child a {
+            border-right: none;
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a aria-curent="page" class="active" href="/">Start Page</a></li>
+            <li><a href="/table">Database Viewer</a></li>
+            <li><a href="https://hydephp.github.io/">Back to Hyde
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAV0lEQVR4Xq2QwQ2AAAwC3cmd2Kk7sRP64CEJ9qOX8OPatMc/QKppnEPhTmJh23CLiwAqIw21CybKQ28qQi37WGFYBJcwfJQpP8LlEHKyZMF0IdmF13zlAjZ/6H4wb+mUAAAAAElFTkSuQmCC"
+                alt="External link"></a></li>
+        </ul>
+    </nav>
+
     <header style="text-align: center; margin: 60px 40px;">
         <h1>HydePHP Statistics</h1>
         <h2>
